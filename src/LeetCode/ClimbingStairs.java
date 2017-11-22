e7a8b3d66b2321b10e4d4d6885e3ae7f82dc3faf
lzwhhh <li495469217@163.com>
2017-11-22 19:55:18 +0800

    2017-11-22 LeetCode 动态规划

--- src/LeetCode/ClimbingStairs.java
+++ src/LeetCode/ClimbingStairs.java
@@ -2,6 +2,8 @@ package LeetCode;
 
 /**
  * Created by Administrator on 2017/11/21.
+ * dynamic programming
+ *
  * You are climbing a stair case. It takes n steps to reach to the top.
  *
  * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
@@ -9,11 +11,31 @@ package LeetCode;
  * Note: Given n will be a positive integer.
  */
 public class ClimbingStairs {
-    public static void main(String[] args){
-        System.out.println(climbStairs(3));
+    public static void main(String[] args) {
+        System.out.println(climbStairs(44));
     }
 
     public static int climbStairs(int n) {
-
+        //速度太慢
+        /*        if(n<=2){
+            return n;
+        }else{
+            return climbStairs(n-1)+climbStairs(n-2);
+        }*/
+        //循环
+        if(n<3) return n;
+        int preOne = 1;
+        int preTwo = 2;
+        boolean IsOne = true;
+        for (int i = 3; i < n; i++) {
+            if (IsOne) {
+                preOne = preOne + preTwo;
+                IsOne = !IsOne;
+            } else {
+                preTwo = preOne + preTwo;
+                IsOne = !IsOne;
+            }
+        }
+        return preOne+preTwo;
     }
 }

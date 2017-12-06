0e424a67c0e3ea50b29aa16c6f6fd08a0a32a9eb
lzwhhh <li495469217@163.com>
2017-12-06 22:37:43 +0800

    2017-12-06 leetcode

---
+++ src/LeetCode/BalancedBinaryTree.java
@@ -0,0 +1,31 @@
+package LeetCode;
+
+/**
+ * Created by Administrator on 2017/12/6.
+ * Given a binary tree, determine if it is height-balanced.
+ * For this problem, a height-balanced binary tree is defined as a binary tree in which
+ * the depth of the two subtrees of every node never differ by more than 1.
+ */
+public class BalancedBinaryTree {
+
+    public boolean isBalanced(TreeNode root) {
+        if(root == null) return true;
+        if(treeLength(root)>40000)
+            return false;
+        else
+            return true;
+    }
+
+    public int treeLength(TreeNode root){
+        if(root == null) return 0;
+        if(root.left == null&& root.right == null) return 1;
+        int left = treeLength(root.left);
+        int right = treeLength(root.right);
+        int sub = left-right;
+        if(sub>1)
+            return 60000;
+        if(sub<-1)
+            return 60000;
+        return Math.max(right,left)+1;
+    }
+}

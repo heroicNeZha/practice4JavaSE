7f8dd7e6cffb924516ee68e0c9e008833a6cb834
lzwhhh <li495469217@163.com>
2018-01-28 21:12:59 +0800

    循环回文完成

--- src/LeetCode/ValidPalindrome.java
+++ src/LeetCode/ValidPalindrome.java
@@ -5,21 +5,21 @@ package LeetCode;
  */
 public class ValidPalindrome {
     public static void main(String[] args){
-        String s = "A man, a plan, a canal: Panama";
+        String s = "ab2a";
         System.out.println(isPalindrome(s));
     }
 
     public static boolean isPalindrome(String s) {
-        if(s.equals("")) return false;
+        if(s.equals("")) return true;
         for(int i = 0,j=s.length()-1;i<j;){
-            if(Character.isLetter(s.charAt(i))) {
-                if (Character.isLetter(s.charAt(j))) {
+            if(Character.isLetterOrDigit(s.charAt(i))) {
+                if (Character.isLetterOrDigit(s.charAt(j))) {
                     if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j)))
                         return false;
                     i++;
                     j--;
                 } else {
-                    j++;
+                    j--;
                 }
             }
             else{

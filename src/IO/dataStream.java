0c12ee2ce5d6fbec25e4f681f87d281d88a0ca6b
lzwhhh <li495469217@163.com>
2018-03-21 22:12:19 +0800

    使用BufferedReader读取

--- src/IO/dataStream.java
+++ src/IO/dataStream.java
@@ -1,5 +1,6 @@
 package IO;
 
+import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,13 +13,31 @@ import java.util.List;
 public class dataStream {
     public static void main(String[] args) {
         dataStream data = new dataStream();
-        data.doit();
+        data.readFile("D:\\Workspace\\IntelliJ IDEA\\practice4JavaSE\\src\\IO\\b3_data.dat");
     }
 
     public void doit() {
         try {
             Path path = Paths.get("D:\\Workspace\\IntelliJ IDEA\\practice4JavaSE\\src\\IO\\b3_data.dat");
-            List<String> lines = Files.readAllLines(path,Charset.defaultCharset());
+            List<String> lines = Files.readAllLines(path, Charset.defaultCharset());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void readFile(String path) {
+        try {
+            File file = new File(path);
+            if (!file.exists() || file.isDirectory())
+                throw new FileNotFoundException();
+            BufferedReader br = null;
+            br = new BufferedReader(new FileReader(file));
+            String temp = null;
+            temp = br.readLine();
+            while (temp != null) {
+                System.out.println(temp);
+                temp = br.readLine();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
